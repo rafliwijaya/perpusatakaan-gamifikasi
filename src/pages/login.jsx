29d2483907b592fdefb98eaf43126ca3a3551aac
Rafli Wijaya <rafliwijaya2005@gmail.com>
2026-03-04 23:56:25 +0700

    login fitur

---
+++ src/pages/login.jsx
@@ -0,0 +1,40 @@
+import { useState } from "react"
+import { supabase } from "../lib/supabase"
+import { useNavigate } from "react-router-dom"
+
+export default function Login() {
+    const navigate = useNavigate()
+
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [error, setError] = useState("")
+    const [loading, setLoading] = useState(flase)
+
+    const handleLogin = async (e) => {
+        e.preventDefault
+        setLoading(true)
+        return
+
+        const {data, error} = await supabase.auth.signInWithPassword ({
+            email,
+            password
+        })
+
+        if(error) {
+            setError(error.message)
+            return
+        }
+
+        navigate("/dashboard")
+    }
+
+    return (
+        <div style={{maxWidth:"500px", margin:"3rem auto"}}>
+            <form handleLogin>
+                <input type="text" />
+            </form>
+        </div>
+    )
+
+
+}

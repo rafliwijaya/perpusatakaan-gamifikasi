aa970311442b1a68682f49834ca255bfa9531e30
Rafli Wijaya <rafliwijaya2005@gmail.com>
2026-03-18 16:00:50 +0700

    updte admin lyout

--- src/pages/admin/admintrasactions.jsx
+++ src/pages/admin/admintrasactions.jsx
@@ -9,7 +9,7 @@ export default function adminbooks() {
     return (
         <div>
         <img src={imgDev} alt="develop" style={{width:"20rem", height:"20rem"}} />
-        <h3>Sabar ya.. fitur nya masih di kembangin :</h3>
+        <h3>Sabar ya.. fitur nya masih di kembangin :)</h3>
         </div>
     )
 }
